2b254391ef0e6d54937e3f61ff2d708a717e8cc0
Michael Callahan <mcalla242@gmail.com>
2023-04-14 10:31:03 -0600

    Feat: Add methods for open and closing races

--- lib/race.rb
+++ lib/race.rb
@@ -4,10 +4,19 @@ class Race
   def initialize(office)
     @office = office
     @candidates = []
+    @is_open = true
   end
 
   def register_candidate!(details)
     @candidates << candidate = Candidate.new(details)
     candidate
+  end
+
+  def open?
+    @is_open
+  end
+
+  def close!
+    @is_open = false
   end
 end
