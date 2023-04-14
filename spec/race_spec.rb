f689b6a96234112b7449f31e70027031b87b78c5
Michael Callahan <mcalla242@gmail.com>
2023-04-14 09:25:05 -0600

    Test: Add testing specs for Race class

---
+++ spec/race_spec.rb
@@ -0,0 +1,37 @@
+require './lib/candidate'
+require './lib/race'
+
+RSpec.describe Race do
+  before(:each) do
+    @race = Race.new("Texas Governor")
+  end
+
+  describe "#initializes" do
+    it "initializes" do
+      expect(@race).to be_an_instance_of(Race)
+    end
+
+    it "initializes with attributes" do
+      expect(@race.office).to eq("Texas Governor")
+      expect(@race.candidates).to eq([])
+    end
+  end
+
+  describe "#register_candidate! && candidates" do
+    before(:each) do
+      @candidate1 = @race.register_candidate!({name: "Diana D", party: :democrat})
+      @candidate2 = @race.register_candidate!({name: "Roberto R", party: :republican})
+
+    end
+
+    it "can create && register candidates" do
+      expect(@candidate1.class).to eq(Candidate)
+      expect(@candidate1.name).to eq("Diana D")
+      expect(@candidate1.party).to eq(:democrat)
+    end
+
+    it "can list all the candidates in the race" do
+      expect(@race.candidates).to eq([@candidate1, @candidate2])
+    end
+  end
+end
